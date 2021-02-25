1941513d6a89910195aab7df0ed82ba39610d25f
Adil Feroz <Adil.Feroz@MyGeorgian.ca>
2021-02-24 20:35:50 -0500

    first commit

---
+++ scraptwitter/cred.py
@@ -0,0 +1,15 @@
+keys = {
+    "Access token" : "123590737-pKjKwRwYBWKvrZVg0ZqCaWlEL1ZXEJiSyxz3o45I",
+ "Access token secret" : "84hzpZVSWOJA8WrAUsHbfaHEP1msB2dOpnV4Zbwor5gNR",
+    "API key" : "c8hormpUzOO8KENNm2a6Lmh2R",
+    "API key secret" : "3fN0rkDRQYDuwzo9OPkhu995qIbV4YlcAi8flYmIi3YbsXwALz",
+    "userID" :"@adilferoz_26"
+  }
+
+redit_keys = {
+    "client_id":"7r7eDxROP6H1-A",
+    "client_secret":"_fdDp0yqhQfjIBa4XsMgLMtLXFuDng",
+    "user_agent":"script by u/adilferoz26",
+    "redirect_uri":"http://localhost:8080",
+    "refresh_token":"824669124741-Z05jS79zKEvannGS6OmvSQygIPKwUg"
+}
